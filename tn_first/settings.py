97f4125f3159b66d9692d6c21112a1624cf26e37
nurzhol.t@gmail.com <nurzhol.t@gmail.com>
2020-04-12 03:39:10 +0600

    static root updated

--- tn_first/settings.py
+++ tn_first/settings.py
@@ -145,10 +145,11 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
+STATIC_ROOT = '/webapps/django_shop/tendernet/static'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, "static"),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
