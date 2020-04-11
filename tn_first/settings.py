9cc188970793f5cb8ebadc0a6900d00a0c08b57e
Hisham Mahmood <45965466+iLearner1@users.noreply.github.com>
2020-04-11 19:09:04 +0500

    Update settings.py

--- tn_first/settings.py
+++ tn_first/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_ROOT = ''
+STATIC_ROOT = '/webapps/django_shop/tendernet/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
 
