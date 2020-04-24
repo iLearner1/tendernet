060191446f569ff247ab192642866fb51aa16fd5
nurzhol.t@gmail.com <nurzhol.t@gmail.com>
2020-04-25 01:28:36 +0600

    Added password reset functionality. Added pagination to lots list in admin panel

--- lots/admin.py
+++ lots/admin.py
@@ -6,6 +6,7 @@ from lots.models import Cities
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'body','city','numb','price','statzakup',  'date','yst','down',)
     prepopulated_fields = {'slug': ('title',)} # new
+    list_per_page = 30
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Cities)
