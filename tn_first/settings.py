18501f02322236869226a081669e9b3c3f38350d
nurzhol <ynurzhol.t@gmail.com>
2020-05-13 21:44:06 +0600

    from end changes 13 may

--- tn_first/settings.py
+++ tn_first/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = "9=&2%kp!g-o#ns78dsswqj44kmivxuh7pk63%czd4hyl57nh_e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = True
 #DEBUG = True
 # ALLOWED_HOSTS = ['78.40.109.22', 'http://www.tendernet.kz', 'http://tendernet.kz', 'https://www.tendernet.kz', 'https://tendernet.kz']
 # ALLOWED_HOSTS = ['tendernet.kz','http://www.tendernet.kz','78.40.109.22', 'http://tendernet.kz','http://localhost','127.0.0.1']
@@ -193,8 +194,8 @@ CACHES = {
     }
 }
 
-#1
-if not DEBUG:
+
+if DEBUG:
    # uncomment for server/ comment for local server
    # now user don't have to commnent uncomment above line everytime just have to change
    # .env file values acroding to user need production or development
