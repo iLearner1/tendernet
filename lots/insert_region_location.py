9bd83a2373773acec769ac80392691d56e4de71b
jahangir08csebuet <jahangir.08.cse@gmail.com>
2020-06-13 16:13:47 +0600

    fix bug in kato list

--- lots/insert_region_location.py
+++ lots/insert_region_location.py
@@ -23,44 +23,23 @@ def read_xls():
              ]
 
     kato_list = {}
-    dup = 0
-    non_dup = 0
     for f in files:
         filename = dirname + "/kato_lists/"  + f
-        print("filename: ", filename)
 
         # To open Workbook
         wb = xlrd.open_workbook(filename)
         sheet_0 = wb.sheet_by_index(0)
 
         for row in range(sheet_0.nrows):
-
             code = str(sheet_0.cell_value(row, 0))
             name = sheet_0.cell_value(row, 1)
 
-            print("code.type: ", type(code))
-            if code == "155200000":
-                print('code == "155200000":')
             if code[2:] != "0000000":
-                if code == "553200000":
-                    print("dupm code: ", code)
-                    dup += 1
-                elif code == "353200000":
-                    print("dupm code: ", code)
-                    dup += 1
-                elif code == "194200000":
-                    print("dupm code: ", code)
-                    dup += 1
-                elif code == "594200000":
-                    print("dupm code: ", code)
-                    print("the duplicate code: ", code)
-                    dup += 1
+
+                if '5532' in code or '3532' in code or '1942' in code or '5942' in code:
+                    print("duplicate")
                 else:
-                    print("not in duplicate")
-                    non_dup += 1
                     if not code in kato_list.keys():
                         kato_list[code[0:4] + "00000"] = name
 
-    print("dup: ", dup)
-    print("non_dup: ", non_dup)
     return kato_list
